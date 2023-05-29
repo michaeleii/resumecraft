16705eec378768bccd5b3a803a51e415de634e56
Michael Lei <michaelleie0ca@gmail.com>
2023-05-29 13:11:19 -0700

    Add education section

--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -26,32 +26,37 @@ function Form() {
   ]);
   return (
     <div className="p-5">
-      <form className="rounded-xl bg-gray-100 p-5 drop-shadow-lg">
+      <form className="rounded-xl bg-white p-5 drop-shadow-lg">
         <div className="flex flex-col gap-5">
-          <div className="flex flex-col gap-3">
-            <h3 className="text-xl font-bold">Personal Information</h3>
-            <div className="flex flex-col gap-2">
-              <label htmlFor="fname">First Name</label>
-              <input
-                type="text"
-                className="rounded-md border border-gray-300 p-2 outline-none focus:border-blue-500"
-                name="fname"
-                id="fname"
-                onChange={(e) => updateFname(e.currentTarget.value)}
-                value={fname}
-              />
+          <div className="flex flex-col gap-3 border-b-2 pb-5">
+            <h3 className="text-xl font-bold tracking-tighter">
+              Personal Information
+            </h3>
+            <div className="grid grid-cols-2 gap-5">
+              <div className="flex flex-col gap-2">
+                <label htmlFor="fname">First Name</label>
+                <input
+                  type="text"
+                  className="rounded-md border border-gray-300 p-2 outline-none focus:border-blue-500"
+                  name="fname"
+                  id="fname"
+                  onChange={(e) => updateFname(e.currentTarget.value)}
+                  value={fname}
+                />
+              </div>
+              <div className="flex flex-col gap-2">
+                <label htmlFor="fname">Last Name</label>
+                <input
+                  type="text"
+                  className="rounded-md border border-gray-300 p-2 outline-none focus:border-blue-500"
+                  name="fname"
+                  id="fname"
+                  onChange={(e) => updateLname(e.currentTarget.value)}
+                  value={lname}
+                />
+              </div>
             </div>
-            <div className="flex flex-col gap-2">
-              <label htmlFor="fname">Last Name</label>
-              <input
-                type="text"
-                className="rounded-md border border-gray-300 p-2 outline-none focus:border-blue-500"
-                name="fname"
-                id="fname"
-                onChange={(e) => updateLname(e.currentTarget.value)}
-                value={lname}
-              />
-            </div>
+
             <div className="flex flex-col gap-2">
               <label htmlFor="title">Title</label>
               <input
@@ -63,27 +68,61 @@ function Form() {
                 value={title}
               />
             </div>
+            <div className="grid grid-cols-2 gap-5">
+              <div className="flex flex-col gap-2">
+                <label htmlFor="address">Address</label>
+                <input
+                  type="text"
+                  className="rounded-md border border-gray-300 p-2 outline-none focus:border-blue-500"
+                  name="address"
+                  id="address"
+                  onChange={(e) => updateAddress(e.currentTarget.value)}
+                  value={address}
+                />
+              </div>
+              <div className="flex flex-col gap-2">
+                <label htmlFor="mobile">Phone Number</label>
+                <input
+                  type="tel"
+                  className="rounded-md border border-gray-300 p-2 outline-none focus:border-blue-500"
+                  name="mobile"
+                  id="mobile"
+                  onChange={(e) => updateMobile(e.currentTarget.value)}
+                  value={mobile}
+                />
+              </div>
+            </div>
+          </div>
+          <div className="flex flex-col gap-3 border-b-2 pb-5">
+            <h3 className="text-xl font-bold tracking-tighter">Education</h3>
             <div className="flex flex-col gap-2">
-              <label htmlFor="address">Address</label>
+              <label htmlFor="school">School</label>
               <input
                 type="text"
                 className="rounded-md border border-gray-300 p-2 outline-none focus:border-blue-500"
-                name="address"
-                id="address"
-                onChange={(e) => updateAddress(e.currentTarget.value)}
-                value={address}
+                name="school"
+                id="school"
               />
             </div>
-            <div className="flex flex-col gap-2">
-              <label htmlFor="mobile">Phone Number</label>
-              <input
-                type="tel"
-                className="rounded-md border border-gray-300 p-2 outline-none focus:border-blue-500"
-                name="mobile"
-                id="mobile"
-                onChange={(e) => updateMobile(e.currentTarget.value)}
-                value={mobile}
-              />
+            <div className="grid grid-cols-2 gap-5">
+              <div className="flex flex-col gap-2">
+                <label htmlFor="startDate">Start Date</label>
+                <input
+                  type="date"
+                  className="rounded-md border border-gray-300 p-2 outline-none focus:border-blue-500"
+                  name="startDate"
+                  id="startDate"
+                />
+              </div>
+              <div className="flex flex-col gap-2">
+                <label htmlFor="endDate">End Date</label>
+                <input
+                  type="date"
+                  className="rounded-md border border-gray-300 p-2 outline-none focus:border-blue-500"
+                  name="endDate"
+                  id="endDate"
+                />
+              </div>
             </div>
           </div>
         </div>
--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,7 +1,9 @@
 function Header() {
   return (
     <header className="flex flex-col items-center gap-5 bg-gray-900 p-10 md:flex-row md:justify-around">
-      <h1 className="text-3xl font-bold text-white">ResumeCraft</h1>
+      <h1 className="text-4xl font-bold tracking-tighter text-white">
+        ResumeCraft
+      </h1>
       <p className="text-xl text-white">
         Beautiful resumes, <br /> made simple.
       </p>
