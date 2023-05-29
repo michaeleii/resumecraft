7bd1279db5362c1b637dd91ec03d5337e3d8ab13
Michael Lei <michaelleie0ca@gmail.com>
2023-05-29 10:36:47 -0700

    Finish header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,4 +1,11 @@
 function Header() {
-  return <div>Header</div>;
+  return (
+    <header className="flex flex-col items-center gap-5 bg-gray-900 p-10 md:flex-row md:justify-around">
+      <h1 className="text-3xl font-bold text-white">ResumeCraft</h1>
+      <p className="text-xl text-white">
+        Beautiful resumes, <br /> made simple.
+      </p>
+    </header>
+  );
 }
 export default Header;
