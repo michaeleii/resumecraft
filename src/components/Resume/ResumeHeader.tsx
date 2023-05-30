8d4d98f3c120e61dc972bda4f8ded68d1791b7a7
Michael Lei <michaelleie0ca@gmail.com>
2023-05-30 09:36:39 -0700

    Remove shallow

--- src/components/Resume/ResumeHeader.tsx
+++ src/components/Resume/ResumeHeader.tsx
@@ -1,17 +1,13 @@
-import { shallow } from "zustand/shallow";
 import useFormStore from "../../store";
 
 function ResumeHeader() {
-  const [fname, lname, title, address, mobile] = useFormStore(
-    (state) => [
-      state.fname,
-      state.lname,
-      state.title,
-      state.address,
-      state.mobile,
-    ],
-    shallow
-  );
+  const [fname, lname, title, address, mobile] = useFormStore((state) => [
+    state.fname,
+    state.lname,
+    state.title,
+    state.address,
+    state.mobile,
+  ]);
   return (
     <header className="flex flex-col gap-2 border-b-2 border-black pb-2">
       <h1 className="text-3xl font-bold tracking-tighter">
@@ -20,9 +16,9 @@ function ResumeHeader() {
 
       <p className="text-xl tracking-tighter">{title}</p>
 
-      <p className="text-lg">{address}</p>
+      <p className="text-sm">{address}</p>
 
-      <p>{mobile}</p>
+      <p className="text-sm">{mobile}</p>
     </header>
   );
 }
