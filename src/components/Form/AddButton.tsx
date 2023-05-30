4d80817759f2bea2dd581d629972ab0c729280b1
Michael Lei <michaelleie0ca@gmail.com>
2023-05-30 10:48:32 -0700

    Add event handler for add button

--- src/components/Form/AddButton.tsx
+++ src/components/Form/AddButton.tsx
@@ -1,6 +1,14 @@
+import { MouseEventHandler } from "react";
+
 function AddButton() {
+  const handleClick: MouseEventHandler = (e) => {
+    e.preventDefault();
+  };
   return (
-    <button className="mt-5 w-32 self-center rounded-lg bg-black py-2 text-white">
+    <button
+      className="mt-5 w-32 self-center rounded-lg bg-black py-2 text-white"
+      onClick={handleClick}
+    >
       Add
     </button>
   );
